5856de87f21b26d2ba9a3dd533a9220e839f60c8
Iman Kari <iman.kari@live.com>
2022-08-30 18:24:01 +0430

    Fixed pipeline

--- G9ScheduleNUnitTest/G9CScheduleManagementNUnitTest.cs
+++ G9ScheduleNUnitTest/G9CScheduleManagementNUnitTest.cs
@@ -18,20 +18,23 @@ namespace G9ScheduleNUnitTest
         [Order(1)]
         public void InitializeG9Schedule()
         {
-            _schedule = new G9CSchedule(); 
+            _schedule = new G9CSchedule();
         }
 
         [Test]
         [Order(2)]
         public void TestScheduleRunPerSecond()
         {
-
             var counter = 0;
             _schedule
                 .AddScheduleAction(() => { counter++; })
                 .SetDuration(TimeSpan.FromSeconds(1))
                 .AddErrorCallBack(exception => { Assert.Fail($"Schedule error!\n{exception.StackTrace}"); });
+
+            TestContext.WriteLine($"Sleep Start DateTime: {DateTime.Now:s}");
             Thread.Sleep(3369);
+            TestContext.WriteLine($"Sleep End DateTime: {DateTime.Now:s}");
+            TestContext.WriteLine($"The count of number is: {counter}");
             if (counter >= 3)
                 Assert.Pass();
             else
